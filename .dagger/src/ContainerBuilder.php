4350b6a391849ded9c806b1db9a192e97f04c8fb
Benjamin Georgeault <git@wedgesama.fr>
2025-09-19 11:27:08 +0200

    Use Dagger for CI

---
+++ .dagger/src/ContainerBuilder.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaggerModule;
+
+use Dagger\Container;
+use Dagger\Directory;
+use function Dagger\dag;
+
+class ContainerBuilder
+{
+    public function php(
+        Directory $source,
+        string $phpVersion,
+    ): Container {
+        $aptCache = dag()->cacheVolume("apt-cache-{$phpVersion}");
+        $composerBin = dag()->container()->from('composer/composer')->file('/usr/bin/composer');
+
+        $container = dag()
+            ->container()
+            ->from("php:{$phpVersion}")
+            ->withMountedCache('/var/cache/apt/archives', $aptCache)
+            ->withExec(['apt-get', 'update'])
+            ->withExec(['apt-get', 'install', '--yes',
+                'git',
+                'zip',
+            ])
+            ->withMountedFile('/usr/bin/composer', $composerBin)
+            ->withEnvVariable('COMPOSER_ALLOW_SUPERUSER', '1')
+            ->withEnvVariable('COMPOSER_BIN', 'php')
+            ->withWorkdir('/bundle')
+            ->withMountedDirectory('/bundle', $source)
+        ;
+
+        if (empty($container->envVariable('PHP_VERSION'))) {
+            $container->withEnvVariable('PHP_VERSION', $phpVersion);
+        }
+
+        return $container;
+    }
+
+    public function symfony(
+        Directory $source,
+        string $phpVersion,
+        string $symfonyVersion,
+        ?Container $phpContainer = null,
+    ): Container {
+        $phpContainer ??= $this->php($source, $phpVersion);
+
+        $composerCache = dag()->cacheVolume("flex-composer-{$phpVersion}");
+
+        return $phpContainer
+            ->withMountedCache('/root/.composer', $composerCache)
+            ->withEnvVariable('SYMFONY_REQUIRE', $symfonyVersion)
+            ->withExec(['composer', 'global', 'config', '--no-plugins', 'allow-plugins.symfony/flex', 'true'])
+            ->withExec(['composer', 'global', 'require', 'symfony/flex'])
+        ;
+    }
+
+    public function symfonyWithVendor(
+        Directory $source,
+        string $phpVersion,
+        string $symfonyVersion,
+        ?Container $symfonyContainer = null,
+    ): Container {
+        $symfonyContainer ??= $this->symfony($source, $phpVersion, $symfonyVersion);
+
+        $vendorCache = dag()->cacheVolume("php-{$phpVersion}-symfony-{$symfonyVersion}-vendor-cache");
+
+        return $symfonyContainer
+            ->withMountedCache('/bundle/vendor', $vendorCache)
+            ->withExec(['composer', 'update'])
+        ;
+    }
+}
---
+++ .dagger/src/StaticObject.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaggerModule;
+
+use Dagger\Attribute\DaggerFunction;
+use Dagger\Attribute\DaggerObject;
+use Dagger\Attribute\Doc;
+use Dagger\Container;
+use function Dagger\dag;
+
+#[DaggerObject]
+#[Doc('All tools to run static code analysis.')]
+class StaticObject
+{
+    public function __construct(
+        private readonly Container $symfonyContainer,
+    ) {
+    }
+
+    #[DaggerFunction]
+    #[Doc('Run PHPStan')]
+    public function phpstan(): Container
+    {
+        return $this->installTool($this->installCs2Pr($this->symfonyContainer), 'phpstan')
+            ->withExec(['sh', '-c', 'tools/phpstan/vendor/bin/phpstan analyze --no-progress --error-format=checkstyle | cs2pr'])
+        ;
+    }
+
+    #[DaggerFunction]
+    #[Doc('Run Psalm')]
+    public function psalm(): Container
+    {
+        return $this->installTool($this->symfonyContainer, 'psalm')
+            ->withExec(['tools/psalm/vendor/bin/psalm', '--no-progress'])
+        ;
+    }
+
+    #[DaggerFunction]
+    #[Doc('Run PHP CS Fixer')]
+    public function cs(): Container
+    {
+        return $this->installTool($this->symfonyContainer, 'php-cs-fixer')
+            ->withExec(['tools/php-cs-fixer/vendor/bin/php-cs-fixer', 'fix', '--dry-run', '--diff'])
+        ;
+    }
+
+    #[DaggerFunction]
+    #[Doc('Run Composer Normalize')]
+    public function composerNormalize(): Container
+    {
+        $phpVersion = $this->symfonyContainer->envVariable('PHP_VERSION');
+        $composerCache = dag()->cacheVolume("composer-normalize-composer-{$phpVersion}");
+
+        return $this->symfonyContainer
+            ->withMountedCache('/root/.composer', $composerCache)
+            ->withExec(['composer', 'global', 'require', '--dev', 'ergebnis/composer-normalize'])
+            ->withExec(['composer', 'global', 'config', 'allow-plugins.ergebnis/composer-normalize', 'true'])
+            ->withExec(['composer', 'normalize', '--dry-run'])
+        ;
+    }
+
+    private function installTool(Container $container, string $tool): Container
+    {
+        $phpVersion = $container->envVariable('PHP_VERSION');
+        $vendorCache = dag()->cacheVolume("{$tool}-vendor-{$phpVersion}");
+
+        return $container
+            ->withMountedCache("/bundle/tools/{$tool}/vendor", $vendorCache)
+            ->withExec(['composer', 'install', "--working-dir=tools/{$tool}"])
+        ;
+    }
+
+    private function installCs2Pr(Container $container): Container
+    {
+        $phpVersion = $container->envVariable('PHP_VERSION');
+        $composerCache = dag()->cacheVolume("cs2pr-composer-{$phpVersion}");
+
+        $path = $container->envVariable('PATH').':/root/.composer/vendor/bin';
+
+        return $container
+            ->withMountedCache('/root/.composer', $composerCache)
+            ->withExec(['composer', 'global', 'require', 'staabm/annotate-pull-request-from-checkstyle'])
+            ->withEnvVariable('PATH', $path)
+        ;
+    }
+}
